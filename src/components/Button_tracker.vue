e29f7564ab71ac7fbe1148a3dc6cf4258c69d763
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-23 11:55:31 +0600

    Header and Button component added

---
+++ src/components/Button_tracker.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>Add</button>
+</template>
+
+
+<script>
+export default {
+  name: 'Button_tracker'
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Header_tracker.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button_tracker></Button_tracker>
+  </header>
+</template>
+
+
+<script>
+import Button_tracker from "@/components/Button_tracker";
+export default {
+  name: 'Header_tracker',
+
+  components: {
+    Button_tracker
+  },
+
+  props: {
+    title: String,
+  }
+
+}
+</script>
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
